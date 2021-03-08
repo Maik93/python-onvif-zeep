24ddcc5a37efcf36925a998cc470efaf65485355
Michael Mugnai <michael.mugnai@gmail.com>
2021-03-08 09:58:23 +0100

    update requirements and setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ here = os.path.abspath(os.path.dirname(__file__))
 version_path = os.path.join(here, 'onvif/version.txt')
 version = open(version_path).read().strip()
 
-requires = ['zeep >= 4.0.0']
+requires = [
+    'zeep >= 4.0.0',
+    'PyYAML >= 5.4.1',
+]
 
 CLASSIFIERS = [
     'Development Status :: 3 - Alpha',
@@ -25,7 +28,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 2",
     "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.8",
 ]
 
 wsdl_files = [os.path.join('wsdl', item) for item in os.listdir('wsdl')]
@@ -38,13 +41,13 @@ setup(
       version=version,
       description='Python Client for ONVIF Camera',
       long_description=open('README.rst', 'r').read(),
-      author='Cherish Chen',
-      author_email='sinchb128@gmail.com',
-      maintainer='sinchb',
-      maintainer_email='sinchb128@gmail.com',
+      author='Michael Mugnai',
+      author_email='michael.mugnai@gmail.com',
+      maintainer='Maik93',
+      maintainer_email='michael.mugnai@gmail.com',
       license='MIT',
       keywords=['ONVIF', 'Camera', 'IPC'],
-      url='http://github.com/quatanium/python-onvif',
+      url='http://github.com/Maik93/python-onvif-zeep',
       zip_safe=False,
       packages=find_packages(exclude=['docs', 'examples', 'tests']),
       install_requires=requires,
